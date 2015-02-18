4c148281ee8071ea8f150362388a44cf5c0895bf
cwahbong <cw.ahbong@gmail.com>
2015-02-18 11:52:10 +0800

    Add exception classes.

---
+++ tgif/exception.py
@@ -0,0 +1,10 @@
+""" All exceptions go here.
+"""
+
+class Friday(Exception):
+    """ Base exception in Friday game.
+    """
+
+class GameOver(Friday):
+    """ Indicats that the game is overed.
+    """
